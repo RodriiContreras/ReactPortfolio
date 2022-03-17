bfb9133a1f4c10f491640e5b4528fff0aaae7db9
unknown <rodrigolcontreras1317@gmail.com>
2022-03-17 14:24:39 -0300

    terminamos proyecto para desktop

--- src/components/ContactMe/Contact/Contact.jsx
+++ src/components/ContactMe/Contact/Contact.jsx
@@ -6,7 +6,40 @@ function Contact() {
     <div id='Contact_Background'>
     <div id='Contact_BackgroundOpacity'></div>
     <h2 id='Contact_H2'>Contact me</h2>
+    <div id='Contact_ContactContainer'>
+    <div id="fcf-form">
+    <h3 className="fcf-h3">Contact</h3>
 
+    <form id="fcf-form-id" class="fcf-form-class" action="https://formsubmit.co/rodrigolcontreras1317@gmail.com" method="POST">
+        
+        <div class="fcf-form-group">
+            <label for="Name" class="fcf-label">Your name</label>
+            <div class="fcf-input-group">
+                <input type="text" id="Name" placeholder='Example: Rodrigo Contreras' name="Name" class="fcf-form-control" required/>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Email" class="fcf-label">Your email address</label>
+            <div class="fcf-input-group">
+                <input type="email" id="Email" placeholder='Example:Rodrigolcontreras1317@gmail.com' name="Email" class="fcf-form-control" required/>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <label for="Message" class="fcf-label">Your message</label>
+            <div class="fcf-input-group">
+                <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+            </div>
+        </div>
+
+        <div class="fcf-form-group">
+            <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
+        </div>
+     </form>
+    </div>
+    
+    </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Layouts from './Layout/Layouts'
 import './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
 const home =() => {
   return (
     <>
      <div id='Content_Grid'>
      <Layouts/>
+     <div id='Home_Background'>
+    
+      <p id='Home_p'>Hi User!</p>
+      <h2 id='Home_h2'>I'm Rodrigo Contreras</h2>
+      <p id='Home_p2'>and <span id='Home_p2Span'>Full-Stack</span> Developer</p>
+   
+   
+   
+     
+     
+     <p id='Home_p3'>See my history</p>
+      <Link id='Home_Link' to='/AboutMe'><FontAwesomeIcon id='Arrow_Icon' icon={faArrowDown}/></Link> 
+
+
+     </div>
      </div>
     </>
   )
